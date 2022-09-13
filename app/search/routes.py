7600ba6f103089ff8c3a5d82c3bcf8326d839528
drewrolla <andrewong007@gmail.com>
2022-09-12 22:05:05 -0400

    fixed querying issue, need to fix save issues

--- app/search/routes.py
+++ app/search/routes.py
@@ -29,7 +29,7 @@ def searchIceCream():
                 'categories': 'icecream, all',
                 'radius': 10000,
                 'sort_by': 'rating',
-                'limit': 5
+                'limit': 50
             }
             res = requests.get(url = ENDPOINT, params=PARAMETERS, headers=API_AUTH)
             if res.ok:
@@ -46,15 +46,15 @@ def searchIceCream():
                         'website': each['url']
                     }
                     icecreamShops.append(my_dict)
-            # try:
-            #     shops = Icecream.query.filter_by(name=shops.name).all()
-            #     if not shops:
-            #         shops = Icecream(my_dict['name'], my_dict['rating'], my_dict['address'], my_dict['location'], my_dict['state'], my_dict['zipcode'], my_dict['img_url'], my_dict['website'])
-            #         shops.save()
-            #     if current_user.shop.filter_by(name=shops.name).first():
-            #         saved = True
-            # except KeyError:
-            #     flash("Sorry, we couldn't find anything for this location!", 'danger')
+            try:
+                shops = Icecream.query.filter_by(name=my_dict['name']).first()
+                if not shops:
+                    shops = Icecream(my_dict['name'], my_dict['rating'], my_dict['address'], my_dict['location'], my_dict['state'], my_dict['zipcode'], my_dict['img_url'], my_dict['website'])
+                    shops.save()
+                if current_user.shop.filter_by(name=shops.name).first():
+                    saved = True
+            except KeyError:
+                flash("Sorry, we couldn't find anything for this location!", 'danger')
         else:
             flash("Sorry, we couldn't find anything for this location!", 'danger')
     return render_template('icecreamsearch.html', form=form, shops=icecreamShops, saved=saved)
@@ -68,8 +68,7 @@ def savedShops():
 # save ice cream shops
 @search.route('/save/<string:icecream_name>')
 def saveIceCream(icecream_name):
-    current_user
-    shops = Icecream.query.filter_by(name=icecream_name).first()
+    shops = Icecream.query.filter_by(name=icecream_name).all()
     current_user.shop.append(shops)
     db.session.commit()
     return redirect(url_for('search.savedShops'))
@@ -84,76 +83,76 @@ def removeIceCream(icecream_name):
 
 
 ######### API ##########
-@search.route('/api/search', methods=["POST"])
-def apiSearch():
-    form = IceCreamSearchForm()
-    my_dict = {}
-    saved = False
-    if request.method == "POST":
-        print('Post request made.')
-        if form.validate():
-            location = form.location.data
+# @search.route('/api/search', methods=["POST"])
+# def apiSearch():
+#     form = IceCreamSearchForm()
+#     my_dict = {}
+#     saved = False
+#     if request.method == "POST":
+#         print('Post request made.')
+#         if form.validate():
+#             location = form.location.data
 
-            # Define the parameters
-            PARAMETERS = {
-                'location': location,
-                'categories': 'icecream',
-                'limit': 50,
-                'radius': 10000
-            }
-            res = requests.get(url = ENDPOINT, params=PARAMETERS, headers=API_AUTH)
-            if res.ok:
-                yelp_data = res.json()
-                for each in yelp_data['businesses']:
-                    my_dict = {
-                        'name': each['name'],
-                        'rating': each['rating'],
-                        'address': each['location']['address1'],
-                        'img_url': each['image_url'],
-                        'website': each['url']
-                    }
-                shops = Icecream.query.filter_by(name=my_dict['name']).first()
-                if not shops:
-                    shops = Icecream(my_dict['name'], my_dict['rating'], my_dict['address'], my_dict['img_url'], my_dict['website'])
-                    shops.save()
-                if current_user.shop.filter_by(name=shops.name).first():
-                    saved = True
-    return {
-        'status': 'ok',
-        'message': 'search completed!'
-    }
+#             # Define the parameters
+#             PARAMETERS = {
+#                 'location': location,
+#                 'categories': 'icecream',
+#                 'limit': 50,
+#                 'radius': 10000
+#             }
+#             res = requests.get(url = ENDPOINT, params=PARAMETERS, headers=API_AUTH)
+#             if res.ok:
+#                 yelp_data = res.json()
+#                 for each in yelp_data['businesses']:
+#                     my_dict = {
+#                         'name': each['name'],
+#                         'rating': each['rating'],
+#                         'address': each['location']['address1'],
+#                         'img_url': each['image_url'],
+#                         'website': each['url']
+#                     }
+#                 shops = Icecream.query.filter_by(name=my_dict['name']).first()
+#                 if not shops:
+#                     shops = Icecream(my_dict['name'], my_dict['rating'], my_dict['address'], my_dict['img_url'], my_dict['website'])
+#                     shops.save()
+#                 if current_user.shop.filter_by(name=shops.name).first():
+#                     saved = True
+#     return {
+#         'status': 'ok',
+#         'message': 'search completed!'
+#     }
 
-@search.route('/api/shops', methods=["POST"])
-@token_required
-def getShops(user):
-    return {
-        'status': 'ok',
-        'shop': [p.to_dict() for p in user.getShops()]
-    }
+# @search.route('/api/shops', methods=["POST"])
+# @token_required
+# def getShops(user):
+#     return {
+#         'status': 'ok',
+#         'shop': [p.to_dict() for p in user.getShops()]
+#     }
 
-@search.route('/api/shops/save', methods=["POST"])
-@token_required
-def saveShop(user):
-    data = request.json
+# @search.route('/api/shops/save', methods=["POST"])
+# @token_required
+# def saveShop(user):
+#     data = request.json
 
-    icecream_id = data['icecreamId']
-    icecream = Icecream.query.get(icecream_id)
+#     icecream_id = data['icecreamId']
+#     icecream = Icecream.query.get(icecream_id)
 
-    user.saveShops(icecream)
-    return {
-        'status': 'ok',
-        'message': 'Saved shop!'
-    }
+#     user.saveShops(icecream)
+#     return {
+#         'status': 'ok',
+#         'message': 'Saved shop!'
+#     }
 
-@search.route('/api/shops/remove', methods=["POST"])
-@token_required
-def removeShop(user):
-    data = request.json
-    icecream_id = data['icecreamId']
-    icecream = Icecream.query.get(icecream_id)
+# @search.route('/api/shops/remove', methods=["POST"])
+# @token_required
+# def removeShop(user):
+#     data = request.json
+#     icecream_id = data['icecreamId']
+#     icecream = Icecream.query.get(icecream_id)
 
-    user.removeShops(icecream)
-    return {
-        'status': 'ok',
-        'message': 'Shop removed!'
-    }
+#     user.removeShops(icecream)
+#     return {
+#         'status': 'ok',
+#         'message': 'Shop removed!'
+#     }
